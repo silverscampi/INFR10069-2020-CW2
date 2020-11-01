00ab3765c5839eb7cd5b4545d2975532d9011eb3
HiroInf2b <65961891+HiroInf2b@users.noreply.github.com>
2020-11-01 18:41:36 +0000

    Add files via upload

---
+++ templates/iaml01cw2_q2.py
@@ -0,0 +1,50 @@
+
+##########################################################
+#  Python script template for Question 2 (IAML Level 10)
+#  Note that
+#  - You should not change the filename of this file, 'iaml01cw2_q2.py', which is the file name you should use when you submit your code for this question.
+#  - You should define the functions shown below in your code.
+#  - You can define function arguments (parameters) and returns (attributes) if necessary.
+#  - In case you define helper functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_helpers.py", and import it in this script.
+#  - For those questions requiring you to show results in tables, your code does not need to present them in tables - just showing them with print() is fine.
+#  - You do not need to include this header in your submission
+##########################################################
+
+#--- Code for loading the data set and pre-processing --->
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#<----
+
+# Q2.1
+def iaml01cw2_q2_1():
+#
+# iaml01cw2_q2_1()   # comment this out when you run the function
+
+# Q2.2
+def iaml01cw2_q2_2():
+#
+# iaml01cw2_q2_2()   # comment this out when you run the function
+
+# Q2.3
+def iaml01cw2_q2_3():
+#
+# iaml01cw2_q2_3()   # comment this out when you run the function
+
+# Q2.4
+def iaml01cw2_q2_4():
+#
+# iaml01cw2_q2_4()   # comment this out when you run the function
+
+# Q2.5
+def iaml01cw2_q2_5():
+#
+# iaml01cw2_q2_5()   # comment this out when you run the function
+
+# Q2.6 
+def iaml01cw2_q2_6():
+#
+# iaml01cw2_q2_6()   # comment this out when you run the function
+
---
+++ templates/iaml01cw2_q3.py
@@ -0,0 +1,45 @@
+
+##########################################################
+#  Python script template for Question 3 (IAML Level 10)
+#  Note that:
+#  - You should not change the filename of this file, 'iaml01cw2_q3.py', which is the file name you should use when you submit your code for this question.
+#  - You should define the functions shown below in your code.
+#  - You can define function arguments (parameters) and returns (attributes) if necessary.
+#  - In case you define helper functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_my_helpers.py", and import it in this script.
+#  - For those questions requiring you to show results in tables, your code does not need to present them in tables - just showing them with print() is fine.
+#  - You do not need to include this header in your submission
+##########################################################
+
+#--- Code for loading the data set and pre-processing --->
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#<----
+
+# Q3.1
+def iaml01cw2_q3_1():
+#
+# iaml01cw2_q3_1()   # comment this out when you run the function
+
+# Q3.2
+def iaml01cw2_q3_2():
+#
+# iaml01cw2_q3_2()   # comment this out when you run the function
+
+# Q3.3
+def iaml01cw2_q3_3():
+#
+# iaml01cw2_q3_3()   # comment this out when you run the function
+
+# Q3.4
+def iaml01cw2_q3_4():
+#
+# iaml01cw2_q3_4()   # comment this out when you run the function
+
+# Q3.5
+def iaml01cw2_q3_5():
+#
+# iaml01cw2_q3_5()   # comment this out when you run the function
+
