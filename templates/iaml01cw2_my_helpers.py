a27a502eea59a783872ba46c4affe882f460b4e1
HiroInf2b <65961891+HiroInf2b@users.noreply.github.com>
2020-11-03 18:37:35 +0000

    Add files via upload

--- templates/iaml01cw2_my_helpers.py
+++ templates/iaml01cw2_my_helpers.py
@@ -3,6 +3,6 @@
 #  Note that:
 #  - Those helper functions of your own for Questions 1, 2, and 3 should be defined in this file.
 #  - You can decide function names by yourself.
-#  - You do not need to include this header in your submission
+#  - You do not need to include this header in your submission.
 ##########################################################
 
--- templates/iaml01cw2_q3.py
+++ templates/iaml01cw2_q3.py
@@ -2,19 +2,23 @@
 ##########################################################
 #  Python script template for Question 3 (IAML Level 10)
 #  Note that:
-#  - You should not change the filename of this file, 'iaml01cw2_q3.py', which is the file name you should use when you submit your code for this question.
-#  - You should define the functions shown below in your code.
+#  - You should not change the name of this file, 'iaml01cw2_q3.py', which is the file name you should use when you submit your code for this question.
+#  - You should write code for the functions defined below. Do not change their names.
 #  - You can define function arguments (parameters) and returns (attributes) if necessary.
-#  - In case you define helper functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_my_helpers.py", and import it in this script.
+#  - In case you define additional functions, do not define them here, but put them in a separate Python module file, "iaml01cw2_my_helpers.py", and import it in this script.
 #  - For those questions requiring you to show results in tables, your code does not need to present them in tables - just showing them with print() is fine.
-#  - You do not need to include this header in your submission
+#  - You do not need to include this header in your submission.
 ##########################################################
 
-#--- Code for loading the data set and pre-processing --->
+#--- Code for loading modules and the data set and pre-processing --->
+# NB: You can edit the following and add code (e.g. code for loading sklearn) if necessary.
+
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import seaborn as sns
+from iaml01cw2_helpers import *
+# from iaml01cw2_my_helpers import *
 
 #<----
 
