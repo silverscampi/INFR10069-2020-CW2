91c3666aa5993d765d2b6dc74593288e85cfd5d8
Silver <naksitrall@hotmail.co.uk>
2020-11-13 16:19:06 +0000

    write more q1.2

--- templates/iaml01cw2_q1.py
+++ templates/iaml01cw2_q1.py
@@ -30,10 +30,83 @@ twoff_tst = np.ones(Xtst.shape) * 255.0
 Xtrn /= twoff_trn
 Xtst /= twoff_tst
 
-means_Xtrn = np.mean(Xtrn, axis=0)
+Xmean = np.mean(Xtrn, axis=0)
+Xtrn_nm = Xtrn - Xmean
+Xtst_nm = Xtst - Xmean
+
+
+# Q1.1
+print("Q1.1")
+print("~~~~~~~~~")
+def iaml01cw2_q1_1():
+    print(Xtrn_nm[0,:][0:4])
+    print()
+    print(Xtrn_nm[0,:][0:4])
+iaml01cw2_q1_1()   # comment this out when you run the function
+print()
+print()
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#dist = numpy.linalg.norm(a-b)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# Q1.2
+print("Q1.2")
+print("~~~~~~~~~")
+def iaml01cw2_q1_2():
+
+    # split instances by class
+    byclass = np.zeros((10, 6000, 784))
+    for i in range(10):
+        byclass[i] = Xtrn[np.where(Ytrn==i)]
+
+    # calculate means for each classs
+    classmeans = np.zeros((10, 784))
+    for i in range(10):
+        classmeans[i] = np.mean(byclass[i], axis=0)
+
+    # calculate euclidean distance between each class's mean and the instances in that class
+    dists = np.zeros((10, 6000))
+    for i in range(10):         # over classes
+        for j in range(6000):   # over instances in each class
+            dists[i][j] = np.linalg.norm(classmeans[i] - byclass[i][j])
+
+    # argsort to find which instances are closest and furthest from class means
+    inds = np.zeros((10, 6000))
+    for i in range(10):
+        inds[i] = np.argsort(dists[i])
+
+    # 
+    grid = np.zeros((10, 5, 784))
+    for i in range(10):
+        # closest two
+        tmpinds = inds[i][:2]
+        tmpinds = tuple(tmpinds.astype(int))
+        grid[i][:2] = byclass[i,tmpinds,:]
+
+        # class mean
+        grid[i][2] = classmeans[i]
+
+        #furthest two
+        tmpinds = inds[i][-2:]
+        tmpinds = tuple(tmpinds.astype(int))
+        grid[i][-2:] = byclass[i,tmpinds,:]
+    
+    
+
+
+
+iaml01cw2_q1_2()   # comment this out when you run the function
+print()
+print()
 
 
 
 
-#<----
+
+
 
