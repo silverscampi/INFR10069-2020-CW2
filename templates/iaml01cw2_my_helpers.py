18201e467c238d56e52c1850174fc49d3aab1bc5
Silver <naksitrall@hotmail.co.uk>
2020-11-18 22:39:44 +0000

    rm unused myhelpers, start planning q3

--- templates/iaml01cw2_my_helpers.py
+++ templates/iaml01cw2_my_helpers.py
@@ -10,33 +10,6 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap='spring'):
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = 'd'
-    thresh = cm.max()/2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
 
 
-# https://gist.github.com/jakevdp/91077b0cae40f8f8244a
-def discrete_cmap(N, base_cmap=None):
-    """Create an N-bin discrete colormap from the specified input map"""
 
-    # Note that if base_cmap is a string or None, you can simply do
-    #    return plt.cm.get_cmap(base_cmap, N)
-    # The following works for string, None, or a colormap instance:
-
-    base = plt.cm.get_cmap(base_cmap)
-    color_list = base(np.linspace(0, 1, N))
-    cmap_name = base.name + str(N)
-    return base.from_list(cmap_name, color_list, N)
--- templates/iaml01cw2_q3.py
+++ templates/iaml01cw2_q3.py
@@ -15,35 +15,120 @@
 
 import numpy as np
 import scipy
+import math
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.svm import SVC
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from iaml01cw2_helpers import *
-# from iaml01cw2_my_helpers import *
+from iaml01cw2_my_helpers import *
 
 #<----
 
+Xtrn, Ytrn, Xtst, Ytst = load_CoVoST2("../data/")
+
+# random_state=1
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q3.1
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Q3.1")
+print("~~~~~~~~~")
 def iaml01cw2_q3_1():
-#
+    kmeans = KMeans(n_clusters=22, random_state=1)
+    kmeans.fit(Xtrn)
+    inertia = kmeans.intertia_
+
+    for i in range(22):
+        print("for label ", i, "\t", (kmeans.labels_ == i).sum())
+
+
 # iaml01cw2_q3_1()   # comment this out when you run the function
+print()
+print()
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q3.2
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Q3.2")
+print("~~~~~~~~~")
 def iaml01cw2_q3_2():
-#
+    # make dataframe combining Xtrn and Ytrn
+
+    # np.mean(xy_trn_df, axis=0).shape = (26,)
+
+
+    # do pca(n_c=2) on THE MEANS
+
+    # on the two PCs, plot the MEAN VECTORS
+
+
+    # on -this same figure-, also plot the cluster centres (transformed into this PC space....)
+
+    # """format it nicely"""
+    # show lang info with name or abbrev or number
+
 # iaml01cw2_q3_2()   # comment this out when you run the function
+print()
+print()
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q3.3
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Q3.3")
+print("~~~~~~~~~")
 def iaml01cw2_q3_3():
-#
+    # get means again as above
+
+
+    # hierarchy with wards
+
+
+    # show dendrogram with orientation='right'
+    # labels for languages on each leaf
 # iaml01cw2_q3_3()   # comment this out when you run the function
+print()
+print()
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q3.4
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Q3.4")
+print("~~~~~~~~~")
 def iaml01cw2_q3_4():
-#
+    # do kmeans(n_clusters=3, random_state=1) FOR EACH LANG CLASS!
+
+    # do hierarchy with ward
+
+    # do hierarchy with single
+
+    # do hierarchy with complete
+
+
+    # plot dendrogram for ward
+
+    # plot dendrogram for single
+
+    # plot dendrogram for complete
+
+
 # iaml01cw2_q3_4()   # comment this out when you run the function
+print()
+print()
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q3.5
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Q3.5")
+print("~~~~~~~~~")
 def iaml01cw2_q3_5():
-#
+    print()
 # iaml01cw2_q3_5()   # comment this out when you run the function
-
+print()
+print()
