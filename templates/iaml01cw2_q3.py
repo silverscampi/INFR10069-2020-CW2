a10b271ee45cdf1151850874176354d347bb7af3
Silver <naksitrall@hotmail.co.uk>
2020-11-19 21:59:55 +0000

    finish q3.3

--- templates/iaml01cw2_q3.py
+++ templates/iaml01cw2_q3.py
@@ -20,6 +20,7 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.svm import SVC
+from scipy.cluster import hierarchy as hc
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -72,62 +73,64 @@ def iaml01cw2_q3_2():
     # get means of each class
     langmeans = np.zeros((22,26))
     for i in range(22):
-            langmeans[i] = np.mean(grouping.get_group(0).iloc[:,:-1])
+            langmeans[i] = np.mean(grouping.get_group(i).iloc[:,:-1])
+
+    print(langmeans)
 
     # do pca(n_c=2) on THE MEANS
     pca = PCA(n_components=2)
     meanspca = pca.fit_transform(langmeans)
 
-# on the two PCs, plot the MEAN VECTORS
-fig = plt.figure()
-ax = fig.add_axes([0.15, 0.11, 0.8, 0.8])
-ax.set_title("Comparison of PCA mean vectors and K-means cluster centres")
+    # on the two PCs, plot the MEAN VECTORS
+    fig = plt.figure()
+    ax = fig.add_axes([0.15, 0.11, 0.8, 0.8])
+    ax.set_title("Comparison of PCA mean vectors and K-means cluster centres")
 
-meanscatter = ax.scatter(meanspca[:,0], meanspca[:,1], c='orange', s=150, label='Mean vectors')
-ax.set_xlabel('PC1')
-ax.set_ylabel('PC2')
+    meanscatter = ax.scatter(meanspca[:,0], meanspca[:,1], c='orange', s=150, label='Mean vectors')
+    ax.set_xlabel('PC1')
+    ax.set_ylabel('PC2')
 
-# on -this same figure-, also plot the cluster centres (transformed into this PC space....)
-centrespca = pca.transform(centres)
-centrescatter = ax.scatter(centrespca[:,0], centrespca[:,1], c='deepskyblue', s=150, label='Cluster centres', alpha=0.2)
+    # on -this same figure-, also plot the cluster centres (transformed into this PC space....)
+    centrespca = pca.transform(centres)
+    centrescatter = ax.scatter(centrespca[:,0], centrespca[:,1], c='deepskyblue', s=150, label='Cluster centres', alpha=0.2)
 
-# """format it nicely"""
-# show lang info with name or abbrev or number
-labels=['Ar', 'Ca', 'Cy', 'De', 'En', 'Es', 'Et', 'Fa', 'Fr', 'Id', 'It', 'Ja', 'Lv', 'Mn', 'Nl', 'Ru', 'Sl', 'Sv', 'Pt', 'Ta', 'Tr', 'Zh']
+    # """format it nicely"""
+    # show lang info with name or abbrev or number
+    labels=['Ar', 'Ca', 'Cy', 'De', 'En', 'Es', 'Et', 'Fa', 'Fr', 'Id', 'It', 'Ja', 'Lv', 'Mn', 'Nl', 'Ru', 'Sl', 'Sv', 'Pt', 'Ta', 'Tr', 'Zh']
 
-# centres french and means english are overlapping
+    # centres french and means english are overlapping
 
-# annotate every mean except for english, catalan, italian, slovenian, turkish
-for i, label in enumerate(labels):
-    if (i!=4 and i!=1 and i!=10 and i!=17 and i!=20):
-        plt.annotate(label, (meanspca[:,0][i], meanspca[:,1][i]), color='maroon', xytext=(-5, -5), textcoords='offset points')
-    
+    # annotate every mean except for english, catalan, italian, slovenian, turkish
+    for i, label in enumerate(labels):
+        if (i!=4 and i!=1 and i!=10 and i!=17 and i!=20):
+            plt.annotate(label, (meanspca[:,0][i], meanspca[:,1][i]), color='maroon', xytext=(-5, -5), textcoords='offset points')
+        
 
-# HORRIBLE
-# annotate english mean
-plt.annotate('En', (meanspca[:,0][4], meanspca[:,1][4]), color='maroon', xytext=(2, -4), textcoords='offset points')
-# annotate catalan mean
-plt.annotate('Ca', (meanspca[:,0][1], meanspca[:,1][1]), color='maroon', xytext=(-8, 0), textcoords='offset points')
-# annotate italian mean
-plt.annotate('It', (meanspca[:,0][10], meanspca[:,1][10]), color='maroon', xytext=(0, -6), textcoords='offset points')
-# annotate Swedish mean
-plt.annotate('Sv', (meanspca[:,0][17], meanspca[:,1][17]), color='maroon', xytext=(-8, -2), textcoords='offset points')
-# annotate turkish mean
-plt.annotate('Tr', (meanspca[:,0][20], meanspca[:,1][20]), color='maroon', xytext=(0, -4), textcoords='offset points')
-
-
-# annotate every centre except for farsi
-for i, label in enumerate(labels):
-    if (i!=7):
-        plt.annotate(label, (centrespca[:,0][i], centrespca[:,1][i]), color='navy', xytext=(-5, -5), textcoords='offset points')
-
-# annotate farsi centre
-plt.annotate('Fa', (centrespca[:,0][7], centrespca[:,1][7]), color='navy', xytext=(-10, -6), textcoords='offset points')
+    # HORRIBLE
+    # annotate english mean
+    plt.annotate('En', (meanspca[:,0][4], meanspca[:,1][4]), color='maroon', xytext=(2, -4), textcoords='offset points')
+    # annotate catalan mean
+    plt.annotate('Ca', (meanspca[:,0][1], meanspca[:,1][1]), color='maroon', xytext=(-8, 0), textcoords='offset points')
+    # annotate italian mean
+    plt.annotate('It', (meanspca[:,0][10], meanspca[:,1][10]), color='maroon', xytext=(0, -6), textcoords='offset points')
+    # annotate Swedish mean
+    plt.annotate('Sv', (meanspca[:,0][17], meanspca[:,1][17]), color='maroon', xytext=(-8, -2), textcoords='offset points')
+    # annotate turkish mean
+    plt.annotate('Tr', (meanspca[:,0][20], meanspca[:,1][20]), color='maroon', xytext=(0, -4), textcoords='offset points')
 
 
-plt.legend()
+    # annotate every centre except for farsi
+    for i, label in enumerate(labels):
+        if (i!=7):
+            plt.annotate(label, (centrespca[:,0][i], centrespca[:,1][i]), color='navy', xytext=(-5, -5), textcoords='offset points')
 
-plt.show()
+    # annotate farsi centre
+    plt.annotate('Fa', (centrespca[:,0][7], centrespca[:,1][7]), color='navy', xytext=(-10, -6), textcoords='offset points')
+
+
+    plt.legend()
+
+    plt.show()
 
 
 # iaml01cw2_q3_2()   # comment this out when you run the function
@@ -142,13 +145,26 @@ print("Q3.3")
 print("~~~~~~~~~")
 def iaml01cw2_q3_3():
     # get means again as above
-
+    # make dataframe
+    x_trn_df = pd.DataFrame(data=Xtrn)
+    y_trn_df = pd.DataFrame(data=Ytrn, columns=['lang'])
+    xy_trn_df = pd.concat([x_trn_df, y_trn_df], axis=1)
+    # group by class
+    grouping = xy_trn_df.groupby(xy_trn_df.lang)
+    # get means of each class
+    langmeans = np.zeros((22, 26))
+    for i in range(22):
+        langmeans[i] = np.mean(grouping.get_group(i).iloc[:,:-1])
+    
 
     # hierarchy with wards
+    wardshc = hc.ward(langmeans)
 
 
     # show dendrogram with orientation='right'
     # labels for languages on each leaf
+    labels = ["Arabic", "Catalan", "Welsh", "German", "English", "Spanish", "Estonian", "Persian", "French", "Indonesian", "Italian", "Japanese", "Latvian", "Mongolian", "Dutch", "Russian", "Slovenian", "Swedish", "Portuguese", "Tamil", "Turkish", "Chinese", ]
+
 # iaml01cw2_q3_3()   # comment this out when you run the function
 print()
 print()
@@ -160,6 +176,7 @@ print()
 print("Q3.4")
 print("~~~~~~~~~")
 def iaml01cw2_q3_4():
+    print()
     # do kmeans(n_clusters=3, random_state=1) FOR EACH LANG CLASS!
 
     # do hierarchy with ward
